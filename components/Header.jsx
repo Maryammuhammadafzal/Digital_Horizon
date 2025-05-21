a8f16ec62cd24dfc8ea1e2eebe57c94f758d927a
maryammuhammadafzal <natiqnagori7@gmail.com>
2025-05-22 00:18:26 +0500

    created header

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,5 +1,6 @@
+"use client";
 import Image from "next/image";
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import Logo from "../public/logo.svg";
 import {
   NavigationMenu,
@@ -12,12 +13,35 @@ import {
 } from "./ui/navigation-menu";
 import Link from "next/link";
 import { Toggle } from "./ui/toggle";
-import { SquareMenu } from "lucide-react";
-
+import { AlignCenter, X } from "lucide-react";
+import Aos from "aos";
+import "aos/dist/aos.css";
 const Header = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  useEffect(() => {
+    Aos.init({
+      duration: 1000,
+      once: true,
+    });
+  });
+let menuRef = useRef(null);
+useEffect(() => {
+  const closeMenuhandler = (event) => {
+    if (menuRef.current && menuRef.current.contains(event.target)) {
+      setIsMenuOpen(false);
+    }
+  };
+
+  document.addEventListener("click", closeMenuhandler);
+
+  return () => {
+    document.removeEventListener("click", closeMenuhandler);
+  };
+}, []);
   return (
     <header className="w-full h-auto flex justify-center items-center">
-      <div className="lg:w-[85%] md:w-[95%] w-[98%] py-8 md:py-12 flex justify-between items-center">
+      <div className="lg:w-[85%] md:w-[95%] w-[98%] py-8 md:py-12 flex justify-between items-center relative">
         <Link href="/" className="w-60 h-[50px]">
           <Image
             src={Logo}
@@ -50,10 +74,21 @@ const Header = () => {
                 <NavigationMenuItem>
                   <NavigationMenuTrigger>Services</NavigationMenuTrigger>
                   <NavigationMenuContent>
-                    <NavigationMenuLink href="/services/development&design" className="w-[200px]">Web Development & Design</NavigationMenuLink>
-                    <NavigationMenuLink href="/services/marketing">Digital Marketing</NavigationMenuLink>
-                    <NavigationMenuLink href="/services/brand">Branding & Identity</NavigationMenuLink>
-                    <NavigationMenuLink href="/services/ecommerce">Ecommerce Solution</NavigationMenuLink>
+                    <NavigationMenuLink
+                      href="/services/development&design"
+                      className="w-[200px]"
+                    >
+                      Web Development & Design
+                    </NavigationMenuLink>
+                    <NavigationMenuLink href="/services/marketing">
+                      Digital Marketing
+                    </NavigationMenuLink>
+                    <NavigationMenuLink href="/services/brand">
+                      Branding & Identity
+                    </NavigationMenuLink>
+                    <NavigationMenuLink href="/services/ecommerce">
+                      Ecommerce Solution
+                    </NavigationMenuLink>
                   </NavigationMenuContent>
                 </NavigationMenuItem>
               </NavigationMenuList>
@@ -61,16 +96,25 @@ const Header = () => {
               <NavigationMenuItem>
                 <NavigationMenuTrigger>Blog</NavigationMenuTrigger>
                 <NavigationMenuContent>
-                  <NavigationMenuLink href="/blog/project" className="w-[200px]">Project Alpha</NavigationMenuLink>
-                  <NavigationMenuLink href="/blog/brand" className="w-[200px]">Brand Beta</NavigationMenuLink>
-                  <NavigationMenuLink href="/blog/tech" className="w-[200px]">Tech Gamma</NavigationMenuLink>
+                  <NavigationMenuLink
+                    href="/blog/project"
+                    className="w-[200px]"
+                  >
+                    Project Alpha
+                  </NavigationMenuLink>
+                  <NavigationMenuLink href="/blog/brand" className="w-[200px]">
+                    Brand Beta
+                  </NavigationMenuLink>
+                  <NavigationMenuLink href="/blog/tech" className="w-[200px]">
+                    Tech Gamma
+                  </NavigationMenuLink>
                 </NavigationMenuContent>
               </NavigationMenuItem>
               {/* Item 5 */}
               <NavigationMenuItem>
                 <Link href="/faq" legacyBehavior passHref>
                   <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-                    FAQ
+                    FAQs
                   </NavigationMenuLink>
                 </Link>
               </NavigationMenuItem>
@@ -85,10 +129,119 @@ const Header = () => {
             </NavigationMenuList>
           </NavigationMenu>
 
-          <Toggle className="md:hidden flex text-2xl">
-                <SquareMenu size={64} className="w-[100px]" />
+          <Toggle
+            onClick={() => setIsMenuOpen((prev) => !prev)}
+            className="md:hidden flex text-2xl cursor-pointer"
+          >
+            <AlignCenter size={64} className="w-[100px] " />
           </Toggle>
         </div>
+
+        {/* Mobile Menu */}
+        {isMenuOpen ? (
+          <div
+            data-aos="fade-left"
+            ref={menuRef}
+            className="mobile-menu absolute -right-2 top-0 p-3 w-[280px] h-screen bg-white shadow-lg"
+          >
+            <Toggle
+              data-aos="fade-right"
+              onClick={() => setIsMenuOpen((prev) => !prev)}
+              className="md:hidden flex text-2xl cursor-pointer"
+            >
+              <X size={64} className="w-[100px] " />
+            </Toggle>
+
+            <NavigationMenu className="w-full py-10 ">
+              <NavigationMenuList className="w-full flex flex-col items-start gap-2 lg:gap-4">
+                {/* Item 1 */}
+                <NavigationMenuItem>
+                  <Link href="/" legacyBehavior passHref>
+                    <NavigationMenuLink
+                      className={navigationMenuTriggerStyle()}
+                    >
+                      Home
+                    </NavigationMenuLink>
+                  </Link>
+                </NavigationMenuItem>
+                {/* Item 2 */}
+                <NavigationMenuItem>
+                  <Link href="/about" legacyBehavior passHref>
+                    <NavigationMenuLink
+                      className={navigationMenuTriggerStyle()}
+                    >
+                      About Us
+                    </NavigationMenuLink>
+                  </Link>
+                </NavigationMenuItem>
+                {/* Item 3 */}
+                <NavigationMenuList>
+                  <NavigationMenuItem>
+                    <NavigationMenuTrigger>Services</NavigationMenuTrigger>
+                    <NavigationMenuContent>
+                      <NavigationMenuLink
+                        href="/services/development&design"
+                        className="w-[200px]"
+                      >
+                        Web Development & Design
+                      </NavigationMenuLink>
+                      <NavigationMenuLink href="/services/marketing">
+                        Digital Marketing
+                      </NavigationMenuLink>
+                      <NavigationMenuLink href="/services/brand">
+                        Branding & Identity
+                      </NavigationMenuLink>
+                      <NavigationMenuLink href="/services/ecommerce">
+                        Ecommerce Solution
+                      </NavigationMenuLink>
+                    </NavigationMenuContent>
+                  </NavigationMenuItem>
+                </NavigationMenuList>
+                {/* Item 4 */}
+                <NavigationMenuItem>
+                  <NavigationMenuTrigger>Blog</NavigationMenuTrigger>
+                  <NavigationMenuContent>
+                    <NavigationMenuLink
+                      href="/blog/project"
+                      className="w-[200px]"
+                    >
+                      Project Alpha
+                    </NavigationMenuLink>
+                    <NavigationMenuLink
+                      href="/blog/brand"
+                      className="w-[200px]"
+                    >
+                      Brand Beta
+                    </NavigationMenuLink>
+                    <NavigationMenuLink href="/blog/tech" className="w-[200px]">
+                      Tech Gamma
+                    </NavigationMenuLink>
+                  </NavigationMenuContent>
+                </NavigationMenuItem>
+                {/* Item 5 */}
+                <NavigationMenuItem>
+                  <Link href="/faq" legacyBehavior passHref>
+                    <NavigationMenuLink
+                      className={navigationMenuTriggerStyle()}
+                    >
+                      FAQs
+                    </NavigationMenuLink>
+                  </Link>
+                </NavigationMenuItem>
+                {/* Item 6 */}
+                <NavigationMenuItem>
+                  <Link href="/contact" legacyBehavior passHref>
+                    <NavigationMenuLink
+                      className={navigationMenuTriggerStyle()}
+                    >
+                      Contact Us
+                    </NavigationMenuLink>
+                  </Link>
+                </NavigationMenuItem>
+              </NavigationMenuList>
+            </NavigationMenu>
+          </div>
+        ) : null}
       </div>
     </header>
   );
